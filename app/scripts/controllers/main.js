1cf7b998841fb0afe2f6b515f701946a162f2b17
Tom Marra <tmarradesign@gmail.com>
2014-08-22 01:46:06 -0700

    work

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,9 @@
  * Controller of the iggApp
  */
 angular.module('iggApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, campaigns) {
+    //Use our Campaigns service to get the data with a promise and then put it in scope
+    campaigns.get().then(function(data){
+        $scope.campaigns = data;
+    });
   });
